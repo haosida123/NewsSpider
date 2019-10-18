89a862e60307a51b3d79ef6b19529ee1ba3ba299
Sida <haosida123@users.noreply.github.com>
2019-10-18 22:53:26 +0800

    detailed and brief mode

--- NewsSpider.py
+++ NewsSpider.py
@@ -14,7 +14,10 @@ def StringListSave(save_path, filename, slist):
     with open(path, "w+", encoding='utf-8') as fp:
         for s in slist:
             # fp.write("%s\t\t%s\n" % (s[0].encode("utf8"), s[1].encode("utf8")))
-            fp.write("%s\t\t%s\n" % (s[0], s[1]))
+            if len(s) == 2:
+                fp.write("%s\t\t%s\n" % (s[0], s[1]))
+            else:
+                fp.write("%s\t\t%s\t\t%s\n" % (s[0], s[1], s[2]))
 
 def Page_Info(myPage):
     '''Regex'''
@@ -33,7 +36,16 @@ def New_Page_Info(new_page):
     new_items = dom.xpath('//tr/td/a/text()')
     new_urls = dom.xpath('//tr/td/a/@href')
     assert(len(new_items) == len(new_urls))
-    return zip(new_items, new_urls)
+    sub_pages = []
+    for new_url in new_urls:
+        try:
+            sub_page = requests.get(new_url).content.decode("gb18030", 'ignore')
+            sub_dom = etree.HTML(sub_page)
+            sub_pages.append(sub_dom.xpath('//div[@class="post_text"]/p/text()'))
+        except Exception as e:
+            continue
+    # print(sub_item)
+    return zip(new_items, new_urls, sub_pages)
 
 def Spider(url):
     i = 0
@@ -46,6 +58,9 @@ def Spider(url):
     StringListSave(save_path, filename, myPageResults)
     i += 1
     for item, url in myPageResults:
+
+        if i<9: i+=1; continue
+
         print("downloading ", url)
         new_page = requests.get(url).content.decode("gbk")
         # new_page = urllib2.urlopen(url).read().decode("gbk")
